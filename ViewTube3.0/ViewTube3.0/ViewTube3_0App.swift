6a4afc47943df5bb3e292c7e8b8b3b88afefcfcc
FAD <fdkjri@gmail.com>
2024-03-30 14:08:59 +0300

    add project

---
+++ ViewTube3.0/ViewTube3.0/ViewTube3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTube3_0App.swift
+//  ViewTube3.0
+//
+//  Created by Пользователь on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewTube3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
